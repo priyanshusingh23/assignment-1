6a1aed82e18d12d82944a8c6c31d81cecf3ab317
priyanshusingh23 <31541116+priyanshusingh23@users.noreply.github.com>
2017-09-01 21:27:23 +0530

    Create "Q3.cpp

---
+++ "Q3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () 
+{
+	int a,b;
+	cout<< "integer a";
+	cin>>a;
+	cout<<"integer b";
+	cin>>b;
+	cout<< "sum of two integer" << a+b << "substraction of two integer"<<a-b <<"multiplication of two integer"<<a*b <<"division of two integer"<<a/b;
+	return 0;
+}
