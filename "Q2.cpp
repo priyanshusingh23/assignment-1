20ec15a46d0f36ee0b578e95e1cded39e78c6c66
priyanshusingh23 <31541116+priyanshusingh23@users.noreply.github.com>
2017-09-01 21:09:02 +0530

    Create "Q2.cpp

---
+++ "Q2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int a,b;
+	cout<< "integer a";
+	cin>>a;
+	cout<<"integer b";
+	cin>>b;
+	cout<< "sum of two integer" << a+b;
+	return 0;
+}
