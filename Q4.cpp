6a9d426cfbde975f1b00a58335b5b12b43f8d6a7
priyanshusingh23 <31541116+priyanshusingh23@users.noreply.github.com>
2017-09-01 21:38:02 +0530

    Create Q4.cpp

---
+++ Q4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () 
+{
+	int l,b;
+	cout<< "length l";
+	cin>> l;
+	cout<< "breadth b";
+	cin >> b;
+	cout<<  "perimeter of rectangle" << 2*(l+b);
+	return 0;
+}
