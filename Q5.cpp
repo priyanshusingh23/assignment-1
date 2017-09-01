2cc651608da17f137901628ccf498b80a6d6f089
priyanshusingh23 <31541116+priyanshusingh23@users.noreply.github.com>
2017-09-01 21:40:27 +0530

    Create Q5.cpp

---
+++ Q5.cpp
@@ -0,0 +1,12 @@
+include <iostream>
+using namespace std;
+int main () 
+{
+	int l,b;
+	cout<< "length l";
+	cin>> l;
+	cout<< "breadth b";
+	cin >> b;
+	cout<<  "area of rectangle" << l*b;
+	return 0;
+}
