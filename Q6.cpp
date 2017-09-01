6d5bb26c6b457dd54a3e6e56d0cbe7beca0c1d72
priyanshusingh23 <31541116+priyanshusingh23@users.noreply.github.com>
2017-09-01 21:52:12 +0530

    Create Q6.cpp

---
+++ Q6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main () 
+{
+	float pi=3.14;
+	int r;
+	cout<< "radius of circle r";
+	cin>> r;
+	cout<< "diameter of circle" << 2*r << "circumference of circle" << 2*pi*r << "area of circle" << pi*r*r;
+	return 0;
+}
